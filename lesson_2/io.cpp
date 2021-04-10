2ce33a7bbd8ca1db4a1063470d3cd04929499d8b
Alessandro Lira <al.lira.co@gmail.com>
2021-04-10 02:10:12 -0700

    Add response for question 2

---
+++ lesson_2/io.cpp
@@ -0,0 +1,17 @@
+// io.cpp
+
+#include <iostream>
+
+
+int readNumber() {
+
+    std::cout << "Enter a number: ";
+    int x {};
+    std::cin >> x;
+
+    return x;
+}
+
+void writeAnswer(int answer) {
+    std::cout << "The answer is " << answer << '\n';
+}
--- lesson_2/main.cpp
+++ lesson_2/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
-#include "add.h"
+// #include "add.h"
+
+int readNumber();
+void writeAnswer(int x);
 
 int main() {
 
-    std::cout << "The sum of 3 and 4 is " << add(3, 4) << '\n';
+    int x{ readNumber()};
+    int y{ readNumber()};
+    writeAnswer(x + y);
     return 0;
 }
