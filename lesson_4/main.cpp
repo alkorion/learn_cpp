7238f350eb6c9102114067e677cd90dc3a85917b
Alessandro Lira <al.lira.co@gmail.com>
2021-06-06 11:44:00 -0700

    Add main file for 4.12 quiz question

---
+++ lesson_4/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+int main() {
+    std::cout << "Enter your full name: ";
+    std::string name{};
+    std::getline(std::cin >> std::ws, name);
+
+    std::cout << "Enter your age: ";
+    int age{};
+    std::cin >> age;
+
+    double age_per_letter {};
+    age_per_letter = static_cast<double>(name.length()) / static_cast<double>(age);
+
+    std::cout << "You've lived " << age_per_letter << " years for each letter in your name.\n";
+}
