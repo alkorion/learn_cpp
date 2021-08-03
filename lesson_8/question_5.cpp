ff8a2220ae15fa3a7a24b086deb008d288e7109a
Alessandro Lira <al.lira.co@gmail.com>
2021-08-03 02:38:00 -0700

    Add 5c solution

--- lesson_8/question_5.cpp
+++ lesson_8/question_5.cpp
@@ -12,6 +12,11 @@ T mult(T x, U y) {
     return x * y;
 }
 
+// write your sub function template here
+template <typename T, typename U>
+auto sub(T x, U y) {
+    return x - y;
+}
  
 int main()
 {
@@ -21,7 +26,13 @@ int main()
     std::cout << '\n';
 
     std::cout << mult(2, 3) << '\n';
-	std::cout << mult(1.2, 3) << '\n';
+    std::cout << mult(1.2, 3) << '\n';
+
+    std::cout << '\n';
+
+	std::cout << sub(3, 2) << '\n';
+	std::cout << sub(3.5, 2) << '\n';
+	std::cout << sub(4, 1.5) << '\n';
  
 	return 0;
 }
