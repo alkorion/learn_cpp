5979a58510d3bcb815121069c50e259aaec7c721
Alessandro Lira <al.lira.co@gmail.com>
2021-08-03 02:14:43 -0700

    Add solution using type aliases

--- lesson_8/convert_2_rad.cpp
+++ lesson_8/convert_2_rad.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
+
+using degrees_t = double;
+using radians_t = double;
  
 namespace constants
 {
-    inline constexpr double pi { 3.14159 };
+    inline constexpr radians_t pi { 3.14159 };
 }
  
-double convertToRadians(double degrees)
+double convertToRadians(degrees_t degrees)
 {
     return degrees * constants::pi / 180;
 }
@@ -13,10 +16,10 @@ double convertToRadians(double degrees)
 int main()
 {
     std::cout << "Enter a number of degrees: ";
-    double degrees{};
+    degrees_t degrees{};
     std::cin >> degrees;
  
-    double radians { convertToRadians(degrees) };
+    radians_t radians { convertToRadians(degrees) };
     std::cout << degrees << " degrees is " << radians << " radians.\n";
  
     return 0;
