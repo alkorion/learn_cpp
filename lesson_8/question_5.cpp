c6f71d6ef608d4d1244fca475f22a82a70b43c13
Alessandro Lira <al.lira.co@gmail.com>
2021-08-03 02:31:17 -0700

    Add 5b solution

--- lesson_8/question_5.cpp
+++ lesson_8/question_5.cpp
@@ -6,11 +6,22 @@ T add(T x, T y) {
     return x + y;
 }
 
+// write your mult function template here
+template <typename T, typename U>
+T mult(T x, U y) {
+    return x * y;
+}
+
  
 int main()
 {
 	std::cout << add(2, 3) << '\n';
 	std::cout << add(1.2, 3.4) << '\n';
+
+    std::cout << '\n';
+
+    std::cout << mult(2, 3) << '\n';
+	std::cout << mult(1.2, 3) << '\n';
  
 	return 0;
 }
