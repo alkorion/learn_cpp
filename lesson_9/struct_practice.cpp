5e1edd535ac526a3d1c1cee3cef4535c8ae21c75
Alessandro Lira <al.lira.co@gmail.com>
2021-08-06 11:24:10 -0700

    Do some renaming

--- lesson_9/struct_practice.cpp
+++ lesson_9/struct_practice.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 
 struct Advertising {
-    int     ads_shown;
-    double  percentage_clicked;
-    double  avg_revenue_per_click;
+    int     ads_shown{};
+    double  percent_clicked{};
+    double  avg_rev_per_click{};
 };
 
-void print_total_revenue(Advertising a) {
-    std::cout << "Total revenue was: $" << a.ads_shown * a.percentage_clicked * a.avg_revenue_per_click << '\n';
+void print_ad_revenue(Advertising ad) {
+    std::cout << "Total revenue was: $" << ad.ads_shown * ad.percent_clicked * ad.avg_rev_per_click << '\n';
 }
 
 int main () {
     Advertising advert{120, 0.1, 1.2};
 
-    print_total_revenue(advert);
+    print_ad_revenue(advert);
     
     return 0;
 }
