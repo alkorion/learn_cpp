890842102af8df69dcd482fb4a775c254b138c43
Alessandro Lira <al.lira.co@gmail.com>
2021-08-06 11:28:47 -0700

    Add function to get values from user

--- lesson_9/struct_practice.cpp
+++ lesson_9/struct_practice.cpp
@@ -6,14 +6,25 @@ struct Advertising {
     double  avg_rev_per_click{};
 };
 
+Advertising avg_rev_per_click() {
+    Advertising temp;
+
+    std::cout << "How many ads were shown today? ";
+    std::cin >> temp.ads_shown;
+    std::cout << "What percentage of ads were clicked? ";
+    std::cin >> temp.percent_clicked;
+    std::cout << "What is the average revenue per ad clicked? ";
+    std::cin >> temp.avg_rev_per_click;
+
+    return temp;
+}
+
 void print_ad_revenue(Advertising ad) {
     std::cout << "Total revenue was: $" << ad.ads_shown * ad.percent_clicked * ad.avg_rev_per_click << '\n';
 }
 
 int main () {
-    Advertising advert{120, 0.1, 1.2};
-
-    print_ad_revenue(advert);
+    print_ad_revenue(avg_rev_per_click());
     
     return 0;
 }
