5fff14e1d47bc85e62fe4d7ea3d0da22218c2ecf
Alessandro Lira <al.lira.co@gmail.com>
2021-08-07 17:49:02 -0700

    Comment quiz solutions

--- lesson_10/arrays_quiz.cpp
+++ lesson_10/arrays_quiz.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <iterator>
 
+// 9.2 Quiz #2
 namespace Animals {
     enum Animals {
         chicken,
@@ -13,10 +15,11 @@ namespace Animals {
 }
 
 int main() {
+    // 9.2 Quiz #1
     double yearly_temps[365]{ 0.0 };
 
+    // 9.2 Quiz #2
     int num_legs[Animals::max_animals]{ 2, 4, 4, 4, 2, 0 };
-
     std::cout << "An elephant has " << num_legs[Animals::elephant] << " legs.\n";
 
     return 0;
