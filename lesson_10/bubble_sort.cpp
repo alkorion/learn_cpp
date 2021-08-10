2da878c31bd8dfeda675d78ed4b0500abf39b9f7
Alessandro Lira <al.lira.co@gmail.com>
2021-08-09 22:34:23 -0700

    Add solution and comments for 10.4 question #4

--- lesson_10/bubble_sort.cpp
+++ lesson_10/bubble_sort.cpp
@@ -7,20 +7,25 @@ int main() {
     int array_size{ static_cast<int>(std::size(array)) };
     bool swap_occurred { false };
 
+    // iterate bubble-sort <array_size> times
     for (int current_iteration{ 0 }; current_iteration < array_size; ++current_iteration) {
 
-        for (int current_index{ 0 }; current_index < array_size - 1; ++current_index) {
-            if (array[current_index] > array[current_index+1]) {
+        // bubble sort elements up to the index last "bubbled" based on iteration
+        for (int current_index{ 0 }; current_index < array_size - (1 + current_iteration); ++current_index) {
+
+            // if adjacent values are decreasing in size, swap them
+            if (array[current_index] > array[current_index + 1]) {
                 std::swap(array[current_index], array[current_index+1]);
                 swap_occurred = true;
             }
         }
 
+        // Add early termination clause if an entire iteration occurs without a swap
         if (!swap_occurred) {
             std::cout << "Early termination on iteration " << current_iteration + 1 << '\n';
             break;
         }
-        else // swap DID occur, so reset flag to "false" for next iteration
+        else // If a swap DID occur, reset the flag to "false" for next iteration
             swap_occurred = false;
     }
 
