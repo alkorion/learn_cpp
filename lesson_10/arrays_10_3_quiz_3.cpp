71f512c106126ba6c77046716882f2088f786658
Alessandro Lira <al.lira.co@gmail.com>
2021-08-09 21:18:19 -0700

    Modify program to use maxIndex instead of maxScore

--- lesson_10/arrays_10_3_quiz_3.cpp
+++ lesson_10/arrays_10_3_quiz_3.cpp
@@ -5,18 +5,18 @@ int main()
 {
     constexpr int scores[]{ 84, 92, 76, 81, 56 };
  
-    int maxScore{ 0 }; // Assume our largest score is 0 to start
+    int maxIndex{ 0 }; // Use index is 0 to start
  
     // now look for a larger score in the full array
     for (int student{ 0 }; student < static_cast<int>(std::size(scores)); ++student)
     {
-        if (scores[student] > maxScore)
+        if (scores[student] > scores[maxIndex])
         {
-            maxScore = scores[student];
+            maxIndex = student;
         }
     }
  
-    std::cout << "The best score was " << maxScore << '\n';
+    std::cout << "The best score was " << scores[maxIndex] << '\n';
  
     return 0;
 }
