9cfe2cbe63d2ad864c198b0d2b131509a6470ed3
Alessandro Lira <al.lira.co@gmail.com>
2021-08-09 22:24:22 -0700

    Add early-termination check

--- lesson_10/bubble_sort.cpp
+++ lesson_10/bubble_sort.cpp
@@ -5,13 +5,23 @@
 int main() {
     int array[]{ 6, 3, 2, 9, 7, 1, 5, 4, 8 };
     int array_size{ static_cast<int>(std::size(array)) };
+    bool swap_occurred { false };
 
     for (int current_iteration{ 0 }; current_iteration < array_size; ++current_iteration) {
+
         for (int current_index{ 0 }; current_index < array_size - 1; ++current_index) {
             if (array[current_index] > array[current_index+1]) {
                 std::swap(array[current_index], array[current_index+1]);
+                swap_occurred = true;
             }
         }
+
+        if (!swap_occurred) {
+            std::cout << "Early termination on iteration " << current_iteration + 1 << '\n';
+            break;
+        }
+        else // swap DID occur, so reset flag to "false" for next iteration
+            swap_occurred = false;
     }
 
     // print the final array
