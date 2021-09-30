c5a7afa2ae78031b1383ffb892c166921b80c0c0
Alessandro Lira <al.lira.co@gmail.com>
2021-09-30 10:08:49 -0700

    Add 10.x question 6.a solution

---
+++ lesson_10/card_game.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+enum class Rank {
+    rank_2,
+    rank_3,
+    rank_4,
+    rank_5,
+    rank_6,
+    rank_7,
+    rank_8,
+    rank_9,
+    rank_10,
+    rank_jack,
+    rank_queen,
+    rank_king,
+    rank_ace,
+
+    max_ranks
+};
+
+enum class Suit {
+    clubs,
+    daimonds,
+    hearts,
+    spades,
+
+    max_suits
+};
+
+int main() {
+    
+    return 0;
+}
