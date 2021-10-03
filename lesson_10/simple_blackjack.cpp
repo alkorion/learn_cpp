d807482031b54e505d2ba6be3a6481d7b1103b0c
Alessandro Lira <al.lira.co@gmail.com>
2021-10-03 14:39:04 -0700

    Implement initial solution for 10.x.7

--- lesson_10/simple_blackjack.cpp
+++ lesson_10/simple_blackjack.cpp
@@ -48,15 +48,109 @@ bool playBlackjack(deck_type deck) {
     
     */
 
-    // temp return
-    return false;
+    constexpr int dealer_max = 17;
+    constexpr int blackjack_max = 21;
+
+    // initialize scores
+    int dealer_score {};
+    int player_score {};
+
+    // initialize dealer and player cards
+    dealer_score += getCardValue(deck[0]);
+    player_score += getCardValue(deck[1]) + getCardValue(deck[2]);
+
+    // create indext to track top of deck
+    int card_index {3}; // start at index 3 since 3 cards have already been drawn
+
+    bool dealer_done {false};
+    bool player_done {false};
+
+    // player turn
+    while (!player_done) {
+        Card current_card { deck[card_index] };
+        
+        // prompt player to hit/stand  
+        std::cout << "Dealer score =\t" << dealer_score << '\n';
+        std::cout << "Player score = \t" << player_score << '\n';
+        std::cout << "Enter '1' for 'hit or '0' for 'stand': ";
+        int player_response{};
+        std::cin >> player_response;
+
+        switch (player_response) {
+            case 0:
+                player_done = true;
+                continue;
+            case 1:
+                player_score += getCardValue(current_card);
+                std::cout << "Player drew a: ";
+                printCard(current_card);
+                std::cout << '\n';
+                ++card_index; // update next card
+                
+                if (player_score > blackjack_max) {
+                    player_done = true;
+                    continue;
+                }
+                continue; 
+            default:
+                std::cout << "Faulty response, please enter '0' or '1'\n";
+        }   
+
+    }
+    if (player_score > blackjack_max) {
+        std::cout << "Player busts with score of " << player_score << '\n';
+        return false;   // player busts and loses
+    }
+
+    // dealer turn
+    while (!dealer_done) {
+        Card current_card { deck[card_index] };
+
+        std::cout << "Dealer score = " << dealer_score << '\n';
+        std::cout << "Player score = " << player_score << '\n';
+        
+        // dealer is under and hits
+        if (dealer_score < dealer_max) {
+            std::cout << "Dealer hits\n";
+            dealer_score += getCardValue(current_card);
+            std::cout << "Dealer drew a: ";
+            printCard(current_card);
+            std::cout << '\n';
+            ++card_index;
+            if (dealer_score > blackjack_max) {
+                dealer_done = true;
+                continue;
+            }
+            continue;
+        }
+        // dealer is over max and stands
+        else {
+            dealer_done = true;
+            continue;
+        }
+    }
+    
+    if (dealer_score > blackjack_max) {
+        std::cout << "Dealer busts with score of " << dealer_score << '\n';
+        return true;   // dealer busts and loses
+    }
+
+    std::cout << "Dealer final score = " << dealer_score << '\n';
+    std::cout << "Player final score = " << player_score << '\n';
+
+    return player_score > dealer_score;
+
 }
 
 int main() {
     deck_type deck = createDeck();
     shuffleDeck(deck);
 
-    playBlackjack(deck);
+    if (playBlackjack(deck))
+        std::cout << "Player Wins!\n";
+    else
+        std::cout << "Player Loses.\n";
+    
 
     return 0;
 }
